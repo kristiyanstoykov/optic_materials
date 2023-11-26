d67bbf0a3638f0df0801f22c1d9140ed939d6d66
kristian <krisistoikov@gmail.com>
2023-11-26 17:20:08 +0200

    Added price and corresponding methods

--- optic_materials/optic_material.cpp
+++ optic_materials/optic_material.cpp
@@ -2,7 +2,7 @@
 
 Optic_Material::Optic_Material() {}
 
-Optic_Material::Optic_Material(std::string type, double width, double diopter, std::string name)
+Optic_Material::Optic_Material(std::string type, double width, double diopter, std::string name, double price)
 {
     if (type.empty())
     {
@@ -24,10 +24,16 @@ Optic_Material::Optic_Material(std::string type, double width, double diopter, s
         throw std::invalid_argument("The name must be 10 symbols!");
     }
 
+    if (price < 0)
+    {
+        throw std::invalid_argument("The price cant be less than 0!");
+    }
+
     this->type = type;
     this->width = width;
     this->diopter = diopter;
     this->name = name;
+    this->price = price;
 }
 
 std::string Optic_Material::getType()
@@ -70,12 +76,24 @@ void Optic_Material::setWidth(double width)
     this->width = width;
 }
 
+double Optic_Material::getPrice()
+{
+    return this->price;
+}
+
+void Optic_Material::setPrice(double price)
+{
+    this->price = price;
+}
+
+
 ostream& Optic_Material::print(ostream& output) const
 {
     output << "Optic material: " << this->name << endl;
     output << "Type: " << this->type << endl;
     output << "Width: " << this->width << endl;
     output << "Diopter: " << this->diopter << endl;
+    output << "Price: " << this->price << endl;
 
     return output;
 }
@@ -96,6 +114,9 @@ istream& Optic_Material::input(istream& input)
     getline(input, line);
     this->diopter = stod(line.substr(line.find(": ") + 2));
 
+    getline(input, line);
+    this->price = stod(line.substr(line.find(": ") + 2));
+
     return input;
 }
 
@@ -105,7 +126,8 @@ void Optic_Material::to_json(json& j) const
         {"name", this->name},
         {"type", this->type},
         {"width", this->width},
-        {"diopter", this->diopter}
+        {"diopter", this->diopter},
+        {"price", this->price}
     };
 }
 
@@ -115,4 +137,5 @@ void Optic_Material::from_json(json& j)
     this->type = j.at("type").get<std::string>();
     this->width= j.at("width").get<double>();
     this->diopter = j.at("diopter").get<double>();
+    this->price = j.at("price").get<double>();
 }
--- optic_materials/optic_material.h
+++ optic_materials/optic_material.h
@@ -14,10 +14,11 @@ private:
     double width;
     double diopter;
     string name;
+    double price;
 
 public:
     Optic_Material();
-    Optic_Material(std::string type, double width, double diopter, std::string name);
+    Optic_Material(std::string type, double width, double diopter, std::string name, double price);
     std::string getType();
     void setType(std::string type);
     double getWidth();
@@ -26,6 +27,8 @@ public:
     void setDiopter(double diopter);
     std::string getName();
     void setName(std::string name);
+    double getPrice();
+    void setPrice(double price);
 
     ostream& print(ostream& output) const override;
     istream& input(istream& input) override;
